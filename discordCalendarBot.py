4bfce1e4d7f3d987dc966dd4cae17957aa9f1d93
Loic Récoupé <loic.recoupe@gmail.com>
2021-11-26 20:37:31 +0100

    login

--- discordCalendarBot.py
+++ discordCalendarBot.py
@@ -8,3 +8,24 @@ from google.oauth2.credentials import Credentials
 from dateutil import parser
 
 SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events.owned', 'https://www.googleapis.com/auth/calendar.events.owned.readonly' ]
+
+def login():
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    return service
